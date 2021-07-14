abac96aac9efc19da46f0be5b25723cd891063fb
Vinicius <viniciusbbbjj@gmail.com>
2021-07-13 22:03:56 -0300

    13 de julho 2021

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,53 @@ void main() async {
   await Firebase.initializeApp();
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  CollectionReference<Map<String, dynamic>> _refCol =
-      firestore.collection("teste3");
-  _refCol.doc().set({"name": "Vinicius"});
+  /*CollectionReference<Map<String, dynamic>> _refCol =
+      firestore.collection("teste");
+
+  _refCol.doc('001').set({"name": "Vinicius"});
+*/
+  /*firestore
+      .collection('noticias')
+      .add({"noticia": "Primeira Noticia", "cidade": "Ipanema"});
+
+  firestore
+      .collection('noticias')
+      .doc("yafTDXwbx40tzNkpdnQw")
+      .set({"cidade": "Taparuba"});*/
+  /*DocumentReference ref =
+      await firestore.collection('noticias').add({"campo1": 2, "campo2": 3});
+
+  print("Referencia de id:" + ref.id);*/
+
+  /*DocumentSnapshot snapshot =
+      await firestore.collection("usuarios").doc("001").get();
+  print((snapshot.data() as dynamic)["nome"]);
+  print((snapshot.data() as dynamic)["idade"]);*/
+
+  //QuerySnapshot querySnapshot = await firestore.collection('usuarios').get();
+  /*DocumentSnapshot snapshot =
+      await firestore.collection("usuarios").doc("001").get();
+  dynamic dados = snapshot.data();
+  print(dados['nome']);*/
+
+  /*QuerySnapshot querySnapshot = await firestore.collection("usuarios").get();
+  print("dados" + querySnapshot.docs.toString());
+
+  for (DocumentSnapshot item in querySnapshot.docs) {
+    dynamic dados = item.data();
+    print(dados["nome"] + ' ' + dados['idade'].toString());
+  }*/
+
+  QuerySnapshot querySnapshot = await firestore
+      .collection("usuarios")
+      .where("idade", isLessThan: 26)
+      .get();
+  print("dados" + querySnapshot.docs.toString());
+
+  for (DocumentSnapshot item in querySnapshot.docs) {
+    dynamic dados = item.data();
+    print(dados["nome"] + ' ' + dados['idade'].toString());
+  }
 
   runApp(MaterialApp(home: Home()));
 }
